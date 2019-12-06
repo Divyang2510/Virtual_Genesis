9fe16eba1a082266923e1bfaed8bbc05238533a7
Divyang2510 <div.thakkar25@gmail.com>
2019-12-06 18:55:22 +0530

    Update Demo.c

--- Demo.c
+++ Demo.c
@@ -3,6 +3,7 @@
 int main()
 {
   int arr[10];
+  int sum =0;
   
   for(int i =0;i<10;i++)
   {
@@ -14,6 +15,13 @@ int main()
     printf("%d",arr[i]);
     printf("\n");
   }
+  
+  for(int i=0; i<10;i++)
+  {
+    sum+=arr[i];
+  }
+  
+  printf("%d\n",sum);
   
   return 0;
 }
